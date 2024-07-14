dc7bec24af391547aa418e83db8f7243f49fc426
Keet Fox <github@foxfilter.foxpaws.net>
2024-07-14 14:27:00 -0400

    Refactor and added send feature

--- include/arppy.h
+++ include/arppy.h
@@ -6,8 +6,27 @@
 #define ARP_REQ 1
 #define ARP_REP 2
 
-const char * print_ip(uint8_t ip[4], char buffer[], int len);
-const char * print_mac(uint8_t mac[6], char buffer[], int len);
+#define ARP_HTYPE_ETHERNET 1
+#define ARP_PTYPE_IP ETHERTYPE_IP;
+
+#define VERBOSITY_NONE 0
+#define VERBOSITY_LOW 1
+#define VERBOSITY_HIGH 2
+
+#define MAC_BYTE_LENGTH 6
+#define MAC_STRING_LENGTH 14
+#define MAC_SEGMENT_LENGTH 4
+#define IP_BYTE_LENGTH  4
+
+#define HEX_DECIMAL_BASE 16
+
+#define WORD_MASK 0xFFFF
+#define BYTE_MASK 0xFF
+
+#define IP_PRINT_BUFFER 32
+#define MAC_PRINT_BUFFER 32
+
+typedef int socktype_t;
 
 typedef struct _arp_packet {
     uint16_t htype;
@@ -15,17 +34,32 @@ typedef struct _arp_packet {
     uint8_t hlen;
     uint8_t plen;
     uint16_t op;
-    uint8_t sender_mac[6];
-    uint8_t sender_ip[4];
-    uint8_t target_mac[6];
-    uint8_t target_ip[4];
+    uint8_t src_mac[6];
+    uint8_t src_ip[4];
+    uint8_t dst_mac[6];
+    uint8_t dst_ip[4];
 } arp_packet;
 
 typedef struct _eth_frame_hdr {
-    uint8_t destination[6];
-    uint8_t source[6];
+    uint8_t dst[6];
+    uint8_t src[6];
     uint16_t type;
 } frame_hdr;
+
+const char * print_ip(uint8_t ip[IP_BYTE_LENGTH], char buffer[], int len);
+const char * print_mac(uint8_t mac[MAC_BYTE_LENGTH], char buffer[], int len);
+static error_t parse_opt(int key, char *arg, struct argp_state *state);
+int parse_mac_address(char *arg, uint8_t mac[MAC_BYTE_LENGTH]);
+unsigned long *parse_ip_list(char *arg, int *count);
+void print_ip_list(unsigned long *list, unsigned int count);
+socktype_t create_socket(char *ifname);
+void set_source_mac(socktype_t sock, char *ifname, char mac[MAC_BYTE_LENGTH], int set_from_interface, int verbose);
+void set_bpf_filter(socktype_t sock, int verbose);
+void print_arp_request(arp_packet *arp);
+void print_arp_reply(arp_packet *arp);
+int check_ip_handled(uint8_t ip[IP_BYTE_LENGTH], unsigned long *iplist, unsigned int ipcount);
+void create_arp_reply(uint8_t srcmac[MAC_BYTE_LENGTH], uint8_t srcip[IP_BYTE_LENGTH], uint8_t dstmac[MAC_BYTE_LENGTH], uint8_t dstip[IP_BYTE_LENGTH], uint8_t *buffer);
+
 
 
 #endif
--- src/arppy.c
+++ src/arppy.c
@@ -35,15 +35,22 @@ const char doc[] = "Arp responder for limited proxy-arping";
 static char arg_doc[] = "INTERFACE IPS";
 
 static struct argp_option options[] = {
-    {"verbose",     'v',    0,              0,  "Verbose output"},
+    {"verbose",     'v',    0,              0,  "Verbose output (specify multiple times for more verbosity)"},
     {"config",      'c',    "FILE",         0,  "Configuration file"},
+    {"mac",         'm',    "MACADDRSS",    0,  "MAC to send (aaaa.aaaa.aaaa)"},
+    {"dry-run",     'd',    0,              0,  "Do not send arp replies"},
     { 0 }
 };
 
 struct arguments {
-    char *args[2];
-    int verbose;
+    char *ifname;
     char *config;
+    int verbose;
+    int use_interface_mac;
+    uint8_t mac[MAC_BYTE_LENGTH];
+    unsigned long *handleips;
+    unsigned int handle_ip_count;
+    int send_replies;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
@@ -51,16 +58,32 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
     switch (key) {
         case 'v':
-            arguments->verbose = 1;
+            arguments->verbose++;
             break;
         case 'c':
             arguments->config = arg;
+            break;
+        case 'd':
+            arguments->send_replies = 0;
+            break;
+        case 'm':
+            if (EXIT_SUCCESS == parse_mac_address(arg, arguments->mac)) {
+                arguments->use_interface_mac = 0;
+            } else {
+                argp_usage(state);
+            }
             break;
         case ARGP_KEY_ARG:
             if (state->arg_num >= 2)
                 argp_usage(state);
 
-            arguments->args[state->arg_num] = arg;
+            if (state->arg_num == 0) {
+                arguments->ifname = arg;
+            } else {
+                if ((arguments->handleips = parse_ip_list(arg, &arguments->handle_ip_count)) == NULL) {
+                    argp_usage(state);
+                }
+            }
             break;
         case ARGP_KEY_END:
             if (state->arg_num < 2)
@@ -73,46 +96,68 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     return 0;
 }
 
-static struct argp argp = { options, parse_opt, arg_doc, doc };
-struct arguments arguments;
+int parse_mac_address(char *arg, uint8_t mac[MAC_BYTE_LENGTH]) {
+    if (strlen(arg) != MAC_STRING_LENGTH) {
+        fprintf(stderr, "cannot parse mac address %s\n", arg);
+        return EXIT_FAILURE;
+    }
+    char *token = strtok(arg, ".");
+    int maccount = 0;
+    while (token != NULL) {
+        if (strlen(token) != MAC_SEGMENT_LENGTH) {
+            fprintf(stderr, "cannot parse mac address %s [%s]\n", arg, token);
+            return EXIT_FAILURE;
+        }
+        uint16_t part = strtoul(token, NULL, HEX_DECIMAL_BASE);
+        mac[maccount++] = (part >> 8) & WORD_MASK;
+        mac[maccount++] = part & BYTE_MASK;
+        token = strtok(NULL, ".");
+    }
+    return EXIT_SUCCESS;
+}
 
-int main(int argc, char **argv) {
-    int sock;
-    char srcip[32], srcmac[32], dstip[32], dstmac[32];
-    unsigned long *checkips = NULL;
-    unsigned int ipcount = 0;
-    unsigned int ifindex = 0;
-    uint8_t ifmac[6];
-
-    arguments.verbose = 0;
-    arguments.config = 0;
-    
-    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+unsigned long *parse_ip_list(char *arg, int *count) {
+    unsigned long *result = NULL;
 
     // Parse the IP addresses
-    char *token = strtok(arguments.args[1], IPTOKEN);
+    char *token = strtok(arg, IPTOKEN);
     while (token != NULL) {
         struct in_addr addr;
         if (inet_pton(AF_INET, token, &addr) != 1) {
             fprintf(stderr, "cannot parse ip address: %s\n", token);
-            exit(EXIT_FAILURE);
+            return 0;
         }
 
-        if (checkips == NULL) {
-            checkips = malloc(sizeof(unsigned long));
-            ipcount = 1;
+        if (result == NULL) {
+            result = malloc(sizeof(unsigned long));
+            *count = 1;
         } else {
-            unsigned long *iptemp = malloc(sizeof(unsigned long) * (ipcount + 1));
-            memmove(iptemp, checkips, sizeof(unsigned long) * ipcount);
-            free(checkips);
-            checkips = iptemp;
-            ipcount++;
+            unsigned long *iptemp = malloc(sizeof(unsigned long) * ((*count) + 1));
+            memmove(iptemp, result, sizeof(unsigned long) * (*count));
+            free(result);
+            result = iptemp;
+            (*count)++;
         }
-        printf("Parsed %x\n", addr.s_addr);
-        checkips[ipcount - 1] = addr.s_addr;
+        
+        result[(*count) - 1] = addr.s_addr;
         token = strtok(NULL, IPTOKEN);
     }
 
+    return result;
+}
+
+void print_ip_list(unsigned long *list, unsigned int count) {
+    for (int i = 0; i < count; i++) {
+        printf("Listening for IP %lu.%lu.%lu.%lu\n", 
+            list[i] & BYTE_MASK, 
+            list[i] >> 8 & BYTE_MASK,
+            list[i] >> 16 & BYTE_MASK,
+            list[i] >> 24 & BYTE_MASK);
+    }
+}
+
+socktype_t create_socket(char *ifname) {
+    socktype_t sock;
     // Create a raw socket
     if ((sock = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ARP))) < 0) {
         perror("failed to get socket");
@@ -120,7 +165,8 @@ int main(int argc, char **argv) {
     }
 
     // Get interface to bind to
-    if ((ifindex = if_nametoindex(arguments.args[0])) == 0) {
+    unsigned int ifindex = 0;
+    if ((ifindex = if_nametoindex(ifname)) == 0) {
         perror("failed to get interface");
         close(sock);
         exit(EXIT_FAILURE);
@@ -129,7 +175,7 @@ int main(int argc, char **argv) {
     // Bind to the interface
     struct sockaddr_ll bindaddr;    
     memset(&bindaddr, 0, sizeof(bindaddr));
-    bindaddr.sll_ifindex = if_nametoindex(arguments.args[0]);
+    bindaddr.sll_ifindex = if_nametoindex(ifname);
     bindaddr.sll_family = AF_PACKET;
     bindaddr.sll_protocol = htons(ETH_P_ALL);
 
@@ -139,20 +185,33 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    // Get our interface MAC for later use
-    struct ifreq req;
-    strcpy(req.ifr_name, arguments.args[0]);
-    if (ioctl(sock, SIOCGIFHWADDR, &req) < 0) {
-        perror("could not get hardware address");
-        exit(EXIT_FAILURE);
+    return sock;
+}
+
+void set_source_mac(socktype_t sock, char *ifname, char mac[MAC_BYTE_LENGTH], int set_from_interface, int verbose) {
+    if (set_from_interface) {        
+        struct ifreq req;
+        strcpy(req.ifr_name, ifname);
+        if (ioctl(sock, SIOCGIFHWADDR, &req) < 0) {
+            perror("could not get hardware address");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < MAC_BYTE_LENGTH; i++) {
+            mac[i] = req.ifr_addr.sa_data[i];
+        }
     }
 
-    for (int i = 0; i < 6; i++) {
-        ifmac[i] = req.ifr_addr.sa_data[i];
+    if (verbose) {
+        char macbuf[MAC_PRINT_BUFFER];        
+        printf("mac address for replies: %s\n", print_mac(mac, macbuf, sizeof(macbuf)));
     }
 
-    if (arguments.verbose)
-        printf("%s MAC: %s\n", arguments.args[0], print_mac(ifmac, dstmac, sizeof(dstmac)));
+}
+
+void set_bpf_filter(socktype_t sock, int verbose) {
+    if (verbose)
+        printf("setting bpf filter for data\n");
 
     // Create a BPF for only ARP packets
     struct sock_fprog bpf = {
@@ -166,77 +225,80 @@ int main(int argc, char **argv) {
         close(sock);
         exit(EXIT_FAILURE);
     }
+}
 
-    // Allocate our raw packet
-    uint8_t *raw = malloc(sizeof(uint8_t) * IP_MAXPACKET);
-    memset(raw, 0, sizeof(uint8_t) * IP_MAXPACKET);
+void print_arp_request(arp_packet *arp) {
+    char ipbuffer[IP_PRINT_BUFFER];
+    char macbuffer[MAC_PRINT_BUFFER];
+    printf("Request: Who has %s,",
+        print_ip(arp->dst_ip, ipbuffer, sizeof(ipbuffer)));
+    printf(" tell %s (%s)\n", 
+        print_ip(arp->src_ip, ipbuffer, sizeof(ipbuffer)),
+        print_mac(arp->src_mac, macbuffer, sizeof(macbuffer)));
+}
 
-    // Pre-calculate some things for efficiency
-    int expected = sizeof(arp_packet) + sizeof(frame_hdr);
-    int offset = sizeof(frame_hdr);
+void print_arp_reply(arp_packet *arp) {
+    char ipbuffer[IP_PRINT_BUFFER];
+    char macbuffer[MAC_PRINT_BUFFER];
+    printf("Reply: Tell %s (%s),", 
+        print_ip(arp->dst_ip, ipbuffer, sizeof(ipbuffer)), 
+        print_mac(arp->dst_mac, macbuffer, sizeof(macbuffer)));
+    printf(" %s is at %s\n",
+        print_ip(arp->src_ip, ipbuffer, sizeof(ipbuffer)),
+        print_mac(arp->src_mac, macbuffer, sizeof(macbuffer)));
 
-    // Keep looping on each packet until there's an error
-    int len = 0;
-    while ((len =  recv(sock, raw, IP_MAXPACKET, 0)) != -1) {
+}
 
-        // Sanity check and shortcut out
-        if (len != expected)
-            continue;
-        
-        // Find the payload parts
-        frame_hdr *frame = (frame_hdr *) raw;
-        arp_packet *arp = (arp_packet *) (raw + sizeof(frame_hdr));
+int check_ip_handled(uint8_t ip[IP_BYTE_LENGTH], unsigned long *iplist, unsigned int ipcount) {
+    // Convert target IP into an unsigned long
+    unsigned long targetip = ip[0] | ip[1] << 8 | ip[2] << 16 | ip[3] << 24;
 
-        // Check for the frame type to ensure it's ARP
-        if (ntohs(frame->type) != ETHERTYPE_ARP) {
-            printf("Not an ARP (%#06x)\n", ntohs(frame->type));
-            continue;
-        }
-
-        // If it's a request, we need to validate that it's in our list of IP's to generate a reply
-        if (ntohs(arp->op) == ARP_REQ) {
-            if (arguments.verbose) {
-                printf("Request: Who has %s (%s),",
-                    print_ip(arp->target_ip, dstip, sizeof(dstip)), 
-                    print_mac(arp->target_mac, dstmac, sizeof(dstmac)));
-                printf(" tell %s (%s)\n", 
-                    print_ip(arp->sender_ip, srcip, sizeof(srcip)),
-                    print_mac(arp->sender_mac, srcmac, sizeof(srcmac)));
-            }
-            // Convert target IP into an unsigned long
-            unsigned long targetip = arp->target_ip[0] | arp->target_ip[1] << 8 | arp->target_ip[2] << 16 | arp->target_ip[3] << 24;
-            for (int i = 0; i < ipcount; i++) {
-                if (targetip == checkips[i]) {
-                    if (arguments.verbose) {
-                        printf("Match\n");
-                        // TODO: ethernet frame, with arp reply inside, call send
-                    }
-                    break;
-                }
-            }
-
-            
-        } else {
-            // TODO: Don't do much with replies
-            if (arguments.verbose) {
-                printf("Reply: Tell %s (%s),", 
-                    print_ip(arp->target_ip, dstip, sizeof(dstip)), 
-                    print_mac(arp->target_mac, dstmac, sizeof(dstmac)));
-                printf(" %s is at %s\n",
-                    print_ip(arp->sender_ip, srcip, sizeof(srcip)),
-                    print_mac(arp->sender_mac, srcmac, sizeof(srcmac)));
-            }
+    // Check the target IP against the ones we're handling
+    for (int i = 0; i < ipcount; i++) {
+        if (targetip == iplist[i]) {
+            return 1;
         }
     }
 
-    close(sock);
-    free(raw);
-    free(checkips);
+    return 0;
 
-    return EXIT_SUCCESS;
 }
 
-const char * print_ip(uint8_t ip[4], char *buffer, int length) {
+void create_arp_reply(
+    uint8_t srcmac[MAC_BYTE_LENGTH], 
+    uint8_t srcip[IP_BYTE_LENGTH], 
+    uint8_t dstmac[MAC_BYTE_LENGTH], 
+    uint8_t dstip[IP_BYTE_LENGTH], 
+    uint8_t *buffer) 
+{
+    // Create a reply frame
+    frame_hdr *hdr = (frame_hdr *) buffer;
+    arp_packet *arp = (arp_packet *) (buffer + sizeof(frame_hdr));
+
+    // Set the other attributes
+    hdr->type = htons(ETHERTYPE_ARP);
+    arp->htype = ARP_HTYPE_ETHERNET;
+    arp->ptype = ARP_PTYPE_IP;
+    arp->hlen = ETHER_ADDR_LEN;
+    arp->plen = sizeof(in_addr_t);
+    arp->op = ARP_REP;
+
+    // Copy in the MAC addresses
+    for (int i = 0; i < MAC_BYTE_LENGTH; i++) {
+        hdr->src[i] = srcmac[i];
+        arp->src_mac[i] = srcmac[i];
+        hdr->dst[i] = dstmac[i];
+        arp->dst_mac[i] = dstmac[i];
+    }
+
+    // Copy in the IP addresses
+    for (int i = 0; i < IP_BYTE_LENGTH; i++) {
+        arp->src_ip[i] = dstip[i];
+        arp->dst_ip[i] = dstip[i];
+    }
+}
+
+const char * print_ip(uint8_t ip[IP_BYTE_LENGTH], char *buffer, int length) {
     // xxx.xxx.xxx.xxx
     memset(buffer, 0, length);
 
@@ -244,10 +306,94 @@ const char * print_ip(uint8_t ip[4], char *buffer, int length) {
     return buffer;
 }
 
-const char * print_mac(uint8_t mac[6], char *buffer, int length) {
+const char * print_mac(uint8_t mac[MAC_BYTE_LENGTH], char *buffer, int length) {
     // xxxx.xxxx.xxxx
     memset(buffer, 0, length);
 
     snprintf(buffer, length - 1, "%02x%02x.%02x%02x.%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     return buffer;
+}
+
+static struct argp argp = { options, parse_opt, arg_doc, doc };
+struct arguments arguments;
+
+int main(int argc, char **argv) {
+    socktype_t sock;
+
+    // Zero the arguments
+    arguments.verbose = 0;
+    arguments.config = 0;
+    arguments.use_interface_mac = 1;
+    arguments.send_replies = 1;
+    arguments.handleips = 0;
+    arguments.handle_ip_count = 0;
+
+    // Parse the arguments    
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    if (arguments.verbose)
+        printf("verbosity: %d\n", arguments.verbose);
+
+    // Create the socket and bind it
+    sock = create_socket(arguments.ifname);
+
+    // Print the addresses we listen for
+    if (arguments.verbose)
+        print_ip_list(arguments.handleips, arguments.handle_ip_count);
+
+    // Set the source mac for replies
+    set_source_mac(sock, arguments.ifname, arguments.mac, arguments.use_interface_mac, arguments.verbose);
+
+    // Set the BPF filter
+    set_bpf_filter(sock, arguments.verbose);
+
+    // Allocate our raw packet
+    uint8_t *raw = malloc(sizeof(uint8_t) * IP_MAXPACKET);
+    memset(raw, 0, sizeof(uint8_t) * IP_MAXPACKET);
+
+    // Keep looping on each packet until there's an error
+    int len = 0;
+    while ((len =  recv(sock, raw, IP_MAXPACKET, 0)) != -1) {
+        // Find the payload parts
+        frame_hdr *frame = (frame_hdr *) raw;
+        arp_packet *arp_request = (arp_packet *) (raw + sizeof(frame_hdr));
+
+        // Check for the frame type to ensure it's ARP
+        if (ntohs(frame->type) != ETHERTYPE_ARP) {
+            printf("Error: Not an ARP (%#06x)\n", ntohs(frame->type));
+            continue;
+        }
+
+        // If it's a request, we need to validate that it's in our list of IP's to generate a reply
+        if (ntohs(arp_request->op) == ARP_REQ) {
+            if (arguments.verbose)
+                print_arp_request(arp_request);
+
+            if (check_ip_handled(arp_request->dst_ip, arguments.handleips, arguments.handle_ip_count)) {
+                static uint8_t reply[sizeof(frame_hdr) + sizeof(arp_packet)];
+                create_arp_reply(arguments.mac, arp_request->dst_ip, arp_request->src_mac, arp_request->src_ip, reply);
+                if (arguments.verbose) { 
+                    printf("Send ");
+                    print_arp_reply((arp_packet*) (reply + sizeof(frame_hdr)));
+                }
+
+                if (arguments.send_replies) {
+                    if (write(sock, reply, sizeof(reply)) < 0) {
+                        perror("could not write reply");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            }
+            
+        } else {
+            if (arguments.verbose == VERBOSITY_HIGH) 
+                print_arp_reply(arp_request);
+        }
+    }
+
+    close(sock);
+    free(raw);
+    free(arguments.handleips);
+
+    return EXIT_SUCCESS;
 }
